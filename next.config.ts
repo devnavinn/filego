4b1b52f6f727db20d0ab6bfa1b7f910c28eb7d7a
Navin Kumar <navinkumar241999@gmail.com>
2026-09-02 13:02:08 +0530

    image support

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/neo062/image/upload/**",
+      },
+    ],
+  },
   async redirects() {
     return [
       {
